8895ee6a008be9b169ba2686d5482eb70cd92165
Ish <43576317+noor1973@users.noreply.github.com>
2025-12-15 18:04:49 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBMSRujHeiQuQ8IhxLbOjJ3hEwDvXInEEI",
-    authDomain: "rosea-3ecae.firebaseapp.com",
-    projectId: "rosea-3ecae",
-    storageBucket: "rosea-3ecae.firebasestorage.app",
-    messagingSenderId: "75900264594",
-    appId: "1:75900264594:web:b80478d88a2bf86594064b",
-    measurementId: "G-R6QFW1EYZC"
-  };
+  apiKey: "AIzaSyBMSRujHeiQuQ8IhxLbOjJ3hEwDvXInEEI",
+  authDomain: "rosea-3ecae.firebaseapp.com",
+  projectId: "rosea-3ecae",
+  storageBucket: "rosea-3ecae.appspot.com",
+  messagingSenderId: "75900264594",
+  appId: "1:75900264594:web:b80478d88a2bf86594064b",
+  measurementId: "G-R6QFW1EYZC"
+};
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
